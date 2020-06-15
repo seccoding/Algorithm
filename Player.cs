b87fb529d45c68ec3d10685f7fc45b448db61c6b
Min Chang Jang <mcjang1116@gmail.com>
2020-06-15 10:18:52 +0900

    완성

--- Player.cs
+++ Player.cs
@@ -66,6 +66,11 @@ namespace Algorithm
 
         private void AStar()
         {
+                                    // U, L, D, R, UL, DL, DR, UR
+            int[] deltaY = new int[] { -1, 0, 1, 0, -1, 1, 1, -1 };
+            int[] deltaX = new int[] { 0, -1, 0, 1, -1, -1, 1, 1 };
+            int[] cost = new int[] { 10, 10, 10, 10, 14, 14, 14, 14 };
+
             // 점수 매기기
             // F = G + H
             // F = 최종점수 (작을 수록 좋음. 경로에 따라 달라짐)
@@ -83,22 +88,70 @@ namespace Algorithm
                 for (int x = 0; x < _board.Size; x++)
                     open[y, x] = Int32.MaxValue;
 
+            Pos[,] parent = new Pos[_board.Size, _board.Size];
+
             // 시작점 발견 (예약 진행)
-            open[PosY, PosX] = Math.Abs(_board.DestY - PosY) + Math.Abs(_board.DestX - PosX);
+            open[PosY, PosX] = 10 * (Math.Abs(_board.DestY - PosY) + Math.Abs(_board.DestX - PosX));
 
             // 오픈 리스트에 있는 정보 중, 가장 좋은 후보를 빠르게 뽑아오기 위한 도구
             PriorityQueue<PQNode> pq = new PriorityQueue<PQNode>();
             pq.Push(new PQNode() { 
-                F = Math.Abs(_board.DestY - PosY) + Math.Abs(_board.DestX - PosX), 
+                F = 10* (Math.Abs(_board.DestY - PosY) + Math.Abs(_board.DestX - PosX)), 
                 G = 0, 
                 Y = PosY, 
                 X = PosX });
+            parent[PosY, PosX] = new Pos(PosY, PosX);
 
-            while (true)
+            PQNode node;
+            int nextY, nextX;
+            while (pq.Count() > 0)
             {
                 //제일 좋은 후보를 찾는다.
+                node = pq.Pop();
 
+                // 동일한 좌표를 여러 경로로 찾아서, 더 빠른 경로로 인해서 이미 방문된 경우 싑
+                if (closed[node.Y, node.X])
+                    continue;
+
+                // 방문한다
+                closed[node.Y, node.X] = true;
+                // 목적지에 도착했으면 바로 종료
+                if (node.Y == _board.DestY && node.X == _board.DestX)
+                    break;
+
+                // 상하좌우 등 이동할 수 있는 좌표인지 확인해서 예약한다.
+                for(int i = 0; i < deltaY.Length; i++)
+                {
+                    nextY = node.Y + deltaY[i];
+                    nextX = node.X + deltaX[i];
+
+                    // 유효범위를 벗어났으면 스킵
+                    if (nextX < 0 || nextX >= _board.Size || nextY < 0 || nextY >= _board.Size)
+                        continue;
+                    // 벽으로 막혀서 갈 수 없으면 스킵
+                    if (_board.Tile[nextY, nextX] == Board.TileType.Wall)
+                        continue;
+                    // 이미 방문한 곳은 스킵
+                    if (closed[nextY, nextX])
+                        continue;
+
+                    // G = 시작점에서 해당 좌표까지 이동하는데 드는 비용 (작을 수록 좋음, 경로에 따라 달라짐)
+                    int g = node.G + cost[i];
+                    // H = 목적지에서 얼마나 가까운지 (작을 수록 좋음, 고정)
+                    int h =  10* (Math.Abs(_board.DestY - nextY) + Math.Abs(_board.DestX - nextX));
+
+                    // 다른 경로에서 더 빠른길을 찾았으면 스킵
+                    if (open[nextY, nextX] < g + h)
+                        continue;
+
+                    // 예약 진행
+                    open[nextY, nextX] = g + h;
+                    pq.Push(new PQNode() { F = 10* (g + h), G = g, Y = nextY, X = nextX });
+                    parent[nextY, nextX] = new Pos(node.Y, node.X);
+                }
             }
+
+            CalcPathFromParent(parent);
         }
 
         private void BFS()
@@ -147,12 +200,18 @@ namespace Algorithm
                 }
             }
 
+            CalcPathFromParent(parent);
+
+        }
+
+        private void CalcPathFromParent(Pos[,] parent)
+        {
             // BFS 역순으로 길 찾기
             int y = _board.DestY;
             int x = _board.DestX;
 
             Pos parentPos;
-            while( parent[y, x].Y != y || parent[y, x].X != x  )
+            while (parent[y, x].Y != y || parent[y, x].X != x)
             {
                 _points.Add(new Pos(y, x));
 
@@ -212,7 +271,13 @@ namespace Algorithm
         public void Update(int deltaTick)
         {
             if ( _lastIndex >= _points.Count )
-                return;
+            {
+                _lastIndex = 0;
+                _points.Clear();
+                _board.Initialze(_board.Size, this);
+                Initialize(1, 1, _board);
+
+            }
 
             _sumTick += deltaTick;
             if ( _sumTick >= MOVE_TICK )
